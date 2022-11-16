63220e9abe110e77c47c78bfa48d87e6e87b2110
M Haidar Hanif <git@mhaidarhanif.com>
2022-11-16 23:47:55 +0700

    feat: recipe model

---
+++ src/models/recipe.ts
@@ -0,0 +1,55 @@
+import localforage from "localforage";
+import { dataRecipes } from "../data/recipes";
+
+import { Recipe, Recipes } from "../types/recipe";
+
+export const setRecipes = (recipes: Recipes) => {
+  return localforage.setItem("recipes", recipes);
+};
+
+export const getRecipes = async () => {
+  // @ts-ignore
+  const existingRecipes: Recipes = await localforage.getItem("recipes");
+
+  if (existingRecipes?.length > 0) {
+    return existingRecipes;
+  } else {
+    setRecipes(dataRecipes);
+    const recipes = await localforage.getItem("recipes");
+    return recipes;
+  }
+};
+
+export const getRecipeById = async (id: number) => {
+  // @ts-ignore
+  const recipes: Recipes = await localforage.getItem("recipes");
+
+  const recipe = recipes.find((recipe) => {
+    return recipe.id === id;
+  });
+
+  return recipe;
+};
+
+export const createNewRecipe = async (recipe: Recipe): Promise<Recipe> => {
+  // @ts-ignore
+  const recipes: Recipes = await getRecipes();
+
+  const lastIndex: number = recipes.length - 1;
+  const lastRecipe: Recipe = recipes[lastIndex];
+  const lastId: number = lastRecipe?.id || 0;
+
+  console.log({ lastIndex, lastRecipe, lastId });
+
+  const newRecipe = {
+    id: lastId + 1,
+    title: recipe.title,
+    description: recipe.description,
+  };
+
+  const newRecipes = [...recipes, newRecipe];
+
+  await setRecipes(newRecipes);
+
+  return newRecipe;
+};
